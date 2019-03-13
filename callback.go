1db2cdacfa5b92b018a0fe256c3e7cdd7ec416fc
norunners <easeyzach@gmail.com>
2019-03-12 23:57:02 -0700

    Upgraded to Go 1.12 and upgraded dependencies. (#32)

--- callback.go
+++ callback.go
@@ -1,7 +1,6 @@
 package vue
 
 import (
-	"fmt"
 	"github.com/gowasm/go-js-dom"
 )
 
@@ -16,22 +15,19 @@ type callback interface {
 
 // addEventListener adds the callback to the element as an event listener unless the type was previously added.
 func (vm *ViewModel) addEventListener(typ string, cb func(dom.Event)) {
-	_, ok := vm.callbacks[typ]
-	if ok {
+	if _, ok := vm.callbacks[typ]; ok {
 		return
 	}
-	vm.comp.el.AddEventListener(typ, false, cb)
+	vm.comp.el.AddEventListener(typ, cb, false)
 	vm.callbacks[typ] = struct{}{}
 }
 
 // vModel is the vue model event callback.
 func (vm *ViewModel) vModel(event dom.Event) {
-	typ := event.Type()
-	field, ok := event.Target().Attributes()[typ]
+	field, ok := findAttrValue(event.Target(), event.Type())
 	if !ok {
-		must(fmt.Errorf("unknown event type: %s", typ))
+		return
 	}
-
 	value := event.Target().Underlying().Get("value").String()
 	vm.Set(field, value)
 	vm.render()
@@ -39,22 +35,20 @@ func (vm *ViewModel) vModel(event dom.Event) {
 
 // vOn is the vue on event callback.
 func (vm *ViewModel) vOn(event dom.Event) {
-	typ := event.Type()
-	method, ok := findMethod(event.Target(), typ)
+	method, ok := findAttrValue(event.Target(), event.Type())
 	if !ok {
-		must(fmt.Errorf("unknown event type: %s", typ))
+		return
 	}
-
 	vm.Call(method)
 }
 
-// findMethod finds the method name from the event type by searching up the dom tree.
-func findMethod(elem dom.Element, typ string) (string, bool) {
+// findAttrValue finds the attribute value from the given key by searching up the dom tree.
+func findAttrValue(elem dom.Element, key string) (string, bool) {
 	if elem == nil {
 		return "", false
 	}
-	if method, ok := elem.Attributes()[typ]; ok {
+	if method, ok := elem.Attributes()[key]; ok {
 		return method, true
 	}
-	return findMethod(elem.ParentElement(), typ)
+	return findAttrValue(elem.ParentElement(), key)
 }
