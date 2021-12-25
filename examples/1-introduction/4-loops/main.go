6a430f9dfa33063f3ac599bb48256d45d80274de
norunners <nilrunners@gmail.com>
2021-12-25 15:14:40 -0800

    Implemented for loop element attributes. (#13)

---
+++ examples/1-introduction/4-loops/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/norunners/vue"
+	"time"
+)
+
+const tmpl = `
+<div>
+  <ol>
+    <li v-for="Todo in Todos">
+      {{ Todo.Text }}
+    </li>
+  </ol>
+</div>
+`
+
+type Data struct {
+	Todos []Todo
+}
+
+type Todo struct {
+	Text string
+}
+
+func Add(context vue.Context) {
+	data := context.Data().(*Data)
+	data.Todos = append(data.Todos, Todo{"Yikes!"})
+}
+
+func main() {
+	data := &Data{
+		Todos: []Todo{
+			{Text: "Learn wasm"},
+			{Text: "Learn vue"},
+			{Text: "Build something awesome"},
+		},
+	}
+
+	comp := vue.New(
+		vue.El("#app"),
+		vue.Template(tmpl),
+		vue.Data(data),
+		vue.Methods(Add),
+	)
+
+	time.AfterFunc(time.Second, func() {
+		comp.Call("Add")
+	})
+	select {}
+}
